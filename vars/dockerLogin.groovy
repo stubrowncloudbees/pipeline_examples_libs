64e68726c58b86c25422bceb8d1305438c3808e8
stu <sbrown@cloudbees.com>
2018-08-16 08:35:58 +0100

    adding in change to fiux varaible

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,6 +1,6 @@
 def call(String credid) {
     withCredentials([usernamePassword(credentialsId: credid, passwordVariable: 'dpassword', usernameVariable: 'duser')]) {
-        sh "docker login -p ${dpassword} -u ${duser}"
+        sh "docker login --password-stdin ${dpassword} -u ${duser}"
 
     }
 }
